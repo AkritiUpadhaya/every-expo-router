94b94c50e16c32822a3dd59582be5d44e142eeb6
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-04-21 22:15:54 +0545

    platform specific module implementation

--- app/(tabs)/(three)/_layout.tsx
+++ app/(tabs)/(three)/_layout.tsx
@@ -1,8 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform, Pressable } from 'react-native'
 import React from 'react'
 import {Drawer} from 'expo-router/drawer'
+import { Link, Slot } from 'expo-router'
 
 export default function threeLayout() {
+    if(Platform.OS==='web'){
+        return(
+            <div>
+
+                <div>
+                <Link href={'/(tabs)/(three)/drawer1'} asChild>
+                <Pressable>
+                <Text>Page1</Text>
+                </Pressable>
+                </Link>
+                 </div>
+
+                <div>
+                <Link href={'/(tabs)/(three)/drawer2'} asChild>
+                <Pressable>
+                <Text>Page2</Text>
+                </Pressable>
+                </Link>
+                </div>
+
+
+                <Slot/>
+                </div>
+                
+
+        )
+    }
+    else
   return(
     <Drawer>
         <Drawer.Screen name='drawer1' options={{headerTitle:'all friends', drawerLabel:'Friends'}}/>
