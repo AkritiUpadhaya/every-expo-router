f6218015612d34b2d4482a76db29656940239f1c
AkritiUpadhaya <upadhayaakriti23@gmail.com>
2025-04-21 18:16:00 +0545

    stack navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { Link, router } from 'expo-router'
 
 export default function Page() {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Button onPress={()=>router.push('/register')} title='Open register'/>
+
+        <Link href={'/register'} asChild>
+        <Button title='Open register with link'/>
+        </Link>
     </View>
   )
 }
